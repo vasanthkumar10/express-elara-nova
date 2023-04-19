c26d35158b586cf33d3d284bc3a4bee9b34598a6
vasanthkumar10 <vasizebron10@gmail.com>
2023-04-19 23:02:48 +0530

    Create, read and validate in mongodb

--- mongodb/app.js
+++ mongodb/app.js
@@ -15,8 +15,91 @@ const mongoose = require("mongoose");
 
 // connection
 mongoose
-  .connect("mongodb://localhost:27017/playground")
+  .connect("mongodb://localhost:27017/playground") // db is not available, it'll create new DB
   .then(() => console.log("connected to MongoDB....."))
   .catch((err) =>
     console.log("Error occured while connecting mongodb....", err)
   );
+
+// schema
+const bookSchema = new mongoose.Schema({
+  name: { type: String, required: true, minlength: 3, maxlength: 50 },
+  author: String,
+  genre: {
+    type: [String],
+    enum: ["finance", "fantasy", "thriller", "comedy", "horror"],
+  },
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean,
+});
+
+// data types -> Number, String, Date, Array, ObjectId, Buffer
+
+// model creation
+const Book = mongoose.model("Book", bookSchema);
+
+async function createBook() {
+  const book = new Book({
+    name: "rich dad poor dad",
+    author: "robert kiyosaki",
+    genre: ["finance"],
+    isPublished: true,
+  });
+
+  try {
+    const result = await book.save();
+    console.log(`Created a book with data - ${result}`);
+  } catch (err) {
+    console.log(`Error occured while creating book -> ${err}`);
+  }
+}
+
+// createBook();
+
+// getAllBooks
+async function getAllBooks() {
+  const books = await Book.find();
+  console.log("Books", books);
+}
+
+// getAllBooks();
+
+async function getBooks(id) {
+  // const books = await Book.find({
+  //   // author: "vasanth",
+  //   // isPublished: false,
+  //   // genre: "comedy",
+  // })
+  //   .sort({ name: -1 }) // +1 -> asc, -1 -> descending
+  //   // .select({ name: 1, author: 1 });
+  //   // .count();
+  //   .limit(3);
+
+  // // AND
+  // const books = await Book.find().and([
+  //   { name: "Harry Potter" },
+  //   { isPublished: true },
+  // ]);
+
+  // OR
+  // const books = await Book.find().or([
+  //   { name: "Harry Potter" },
+  //   { isPublished: true },
+  // ]);
+
+  // regular expressions (regex)
+  // starts -> /^pattern/
+  // const books = await Book.find({ author: /^vas/i });
+
+  // // ends ->  /pattern$/
+  // const books = await Book.find({ author: /ling$/i });
+
+  // contains -> /.*pattern.*/
+  // const books = await Book.find({ author: /.*ow.*/i });
+
+  // by id
+  const books = await Book.find({ _id: id });
+  console.log("books", books);
+}
+
+getBooks("64401b3cbd6732914bcdca95");
